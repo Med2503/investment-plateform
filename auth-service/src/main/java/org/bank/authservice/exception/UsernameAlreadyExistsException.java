ee9d9dae19264bd927f1e803ce5e9d8374ed73cf
med <medbenattia09@gmail.com>
2026-06-30 15:15:09 +0100

    feat(auth-service): add username exception

---
+++ auth-service/src/main/java/org/bank/authservice/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package org.bank.authservice.exception;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+
+    public UsernameAlreadyExistsException(String username) {
+        super("Username " + username + "already exists.");
+    }
+}
