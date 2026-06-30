769184235adce86bc79bc37a8f5189b87e4c96a5
med <medbenattia09@gmail.com>
2026-06-30 15:28:11 +0100

    feat(auth-service): implement user registration service

---
+++ auth-service/src/main/java/org/bank/authservice/service/impl/AuthService.java
@@ -0,0 +1,39 @@
+package org.bank.authservice.service.impl;
+
+
+import lombok.RequiredArgsConstructor;
+import org.bank.authservice.dto.request.RegisterRequest;
+import org.bank.authservice.dto.response.AuthResponse;
+import org.bank.authservice.entity.Role;
+import org.bank.authservice.entity.User;
+import org.bank.authservice.exception.UsernameAlreadyExistsException;
+import org.bank.authservice.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public AuthResponse register(RegisterRequest request) {
+
+        if (userRepository.findByUsername(request.getUsername()).isPresent()) {
+            throw new UsernameAlreadyExistsException(request.getUsername());
+        }
+
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRole(Role.CUSTOMER);
+
+        userRepository.save(user);
+
+        return new AuthResponse("user registered ");
+
+    }
+
+}
