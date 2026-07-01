794365e1990276c36082e85c33f355c654641396
med <medbenattia09@gmail.com>
2026-07-01 13:30:19 +0100

    feat(auth-service):implement JWT service

--- auth-service/src/main/java/org/bank/authservice/security/jwt/JwtService.java
+++ auth-service/src/main/java/org/bank/authservice/security/jwt/JwtService.java
@@ -1,4 +1,78 @@
 package org.bank.authservice.security.jwt;
 
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+@RequiredArgsConstructor
 public class JwtService {
+
+    private final JwtProperties jwtProperties;
+
+
+    public String generateToken(String username) {
+        Date now = new Date();
+        Date expirationDate = new Date(now.getTime() + jwtProperties.getExpiration());
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(now)
+                .expiration(expirationDate)
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+
+    public boolean isTokenValid(String token, String username) {
+        String extractedUsername = extractUsername(username);
+        return extractedUsername.equals(username) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+
+        return extractClaim(
+                token,
+                Claims::getExpiration
+        ).before(new Date());
+    }
+
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        Claims claims = extractAllClaims(token);
+
+        return resolver.apply(claims);
+
+    }
+
+    private Claims extractAllClaims(String token) {
+
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtProperties.getSecret());
+
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
 }
