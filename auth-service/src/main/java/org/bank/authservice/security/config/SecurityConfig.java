496a3977ba4904ec29588861a35caf5b2786f10e
med <medbenattia09@gmail.com>
2026-07-01 18:25:31 +0100

    refactor(auth-service):secure profile endpoint with JWT

--- auth-service/src/main/java/org/bank/authservice/security/config/SecurityConfig.java
+++ auth-service/src/main/java/org/bank/authservice/security/config/SecurityConfig.java
@@ -29,7 +29,8 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("api/v1/auth/**").permitAll()
+                        .requestMatchers("api/v1/auth/register",
+                                "api/v1/auth/login").permitAll()
                         .anyRequest().authenticated()
 
                 )
