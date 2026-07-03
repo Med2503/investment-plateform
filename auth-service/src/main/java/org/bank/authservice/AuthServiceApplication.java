3e4e125fd0e16403c546d1e98ce0c0932b01489f
med <medbenattia09@gmail.com>
2026-07-03 13:46:39 +0100

    fix(api-gateway):correct auth route mismach with auth-service

--- auth-service/src/main/java/org/bank/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/org/bank/authservice/AuthServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableDiscoveryClient
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
