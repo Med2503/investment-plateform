0f062a0436432e22c9fda062b4fe1db5391b578d
med <medbenattia09@gmail.com>
2026-07-03 10:42:18 +0100

    feat(discovery-service):register services with eureka successfully

--- api-gateway/src/test/java/org/bank/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/org/bank/apigateway/ApiGatewayApplicationTests.java
@@ -1,5 +1,6 @@
 package org.bank.apigateway;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
--- auth-service/src/main/java/org/bank/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/org/bank/authservice/AuthServiceApplication.java
@@ -3,9 +3,11 @@ package org.bank.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableDiscoveryClient
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
