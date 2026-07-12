1cc0436e5479bad828286976795f4efa345fff03
med <medbenattia09@gmail.com>
2026-07-12 14:08:03 +0100

    feat(transfer-service):add transfer domain model

---
+++ transfer-service/src/main/java/org/bank/transferservice/entity/Transfer.java
@@ -0,0 +1,45 @@
+package org.bank.transferservice.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "transfers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Transfer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID sourceAccountId;
+
+
+    @Column(nullable = false)
+    private UUID destinationAccountId;
+
+
+    @Column(nullable = false, scale = 4, precision = 19)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private String currency;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransferStatus status;
+
+    private Instant createdAt;
+
+    private String failureReason;
+}
---
+++ transfer-service/src/main/java/org/bank/transferservice/entity/TransferStatus.java
@@ -0,0 +1,7 @@
+package org.bank.transferservice.entity;
+
+public enum TransferStatus {
+    PENDING,
+    COMPLETED,
+    FAILED
+}
